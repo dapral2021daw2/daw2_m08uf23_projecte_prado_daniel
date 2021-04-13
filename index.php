bd2ec13487d09d6cfa2114763eb2819c7ca970cc
dapral <dapral@zend-dapral.fjeclot.net>
2021-04-13 19:37:11 +0200

    Proyecto Daniel Prado

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+			PÀGINA WEB INICIAL DE L'APLICACIÓ
+		</title>
+	</head>
+	<body>
+		<h1> APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h1>
+		<h2> DAW2 M08UF2 M08UF3 </h2>
+		<h3> Autor: dapral2021daw2</h3>
+		<h3> Correu: dapral2021daw2@protonmail.com</h3>
+		<h3> Github: https://github.com/dapral2021daw2/.......git</h3>
+		<a href="http://zend-dapral.fjeclot.net/practica/login.php">Inicia sessió</a>
+	</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,14 @@
+
+<html>
+	<head>
+		<title>MENU</title>
+	</head>
+	<body>
+		Añadir usuarios <button><a href="anadir.php">Formulario de creación</a></button> <br><br>
+		    Visualización de usuarios <button><a href="visualizar.php">Formulario de visualización</a></button> <br><br>
+		Borrar usuarios <button><a href="borrar.php">Formulario de borrar</a></button> <br><br>
+		Modificación de usuarios <button><a href="modificar.php">Formulario de modificación</a></button> <br><br><br><br>
+		
+		Cerrar sessión <button><a href="index.php">Logout</a></button> <br><br>
+	</body>
+</html>
